6e631fc8a0b49553e837b88ea8ae670587f518c7
Vanness Yang <vanness.yang21@ycdsbk12.ca>
2020-12-04 15:49:39 +0000

    Green PS1

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -5,9 +5,10 @@ public class Recursion{
     // Base Case
     if(str.equals("")){
       return 0; 
+    }else if(str.substring(0,1).equals("h") && str.substring(1,2).equals("i")){
+      return 1 + countHi(str.substring(2));
     }
-    return(100);
-
+    return 100;
   }
 }
 
