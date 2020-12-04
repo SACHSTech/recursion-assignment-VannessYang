604f6c95525005e221553ca51cddf18c77ea4755
Vanness Yang <vanness.yang21@ycdsbk12.ca>
2020-12-04 15:13:16 +0000

    Red Phase PS1

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -1,5 +1,11 @@
 package recursionAssignment;
 
 public class Recursion{
-
+  public static int countHi(String str){
+    // Base Case
+    if(str.equals("")){
+      return 0; 
+    }
+  }
 }
+
--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -1,34 +1,18 @@
 package recursionAssignment;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Before;
-import org.junit.jupiter.api.After;
 
 import recursionAssignment.Recursion;
 
 public class RecursionTest{
     
-    @Before
-    public void beforeTest(){
-      // some steps to perform before tests are run
-    }
-    
     /**
-     * An initial test
+     * Problem Set 1: Testing base case 
      */
     @Test
     public void Test1(){
-        // make assertion statement(s)
-        // assertEquals(EXPECTEDVALUE, TESTVALUE);
-        // assertTrue(TESTBOOLEAN);
+      assertEquals(0, countHi(""));
     }
-    
-    // add more tests
-    
-    @After
-    public void afterTest(){
-      // some steps to perform after tests are run
-    }
-    
     
 }
