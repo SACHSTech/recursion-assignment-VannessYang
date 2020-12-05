96fd7a496bcee47e83a8c09d4ba27a2bed258390
Vanness Yang <vanness.yang21@ycdsbk12.ca>
2020-12-05 18:38:10 +0000

    Refactor PS1

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -5,6 +5,7 @@ public class Recursion{
     // Base Case
     if(str.equals("")){
       return 0; 
+    // Recursive Step 
     }else if(str.substring(0,1).equals("h") && str.substring(1,2).equals("i")){
       return 1 + countHi(str.substring(2));
     }
