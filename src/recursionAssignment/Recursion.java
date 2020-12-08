7da9130e2c93d4152a32504770829ed95866927b
Vanness Yang <vanness.yang21@ycdsbk12.ca>
2020-12-08 23:37:07 +0000

    Refactor Everything

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -6,7 +6,7 @@ public class Recursion{
     if (str.equals("")) {
       return 0; 
     // Recursive Step if hi 
-    }else if (str.substring(0,1).equals("h") && str.substring(1,2).equals("i")) {
+    }else if (str.substring(0, 1).equals("h") && str.substring(1, 2).equals("i")) {
       return 1 + countHi(str.substring(2));
     // Recursive Step if no hi 
     }else{
@@ -19,11 +19,11 @@ public class Recursion{
     if (str.equals("")) {
       return "";
     // Recursive Step if there is an x 
-    }else if (str.substring(0,1).equals("x")){
+    }else if (str.substring(0, 1).equals("x")){
       return endX(str.substring(1)) + "x";
     // Recursive Step if there is no x 
     }else{
-      return str.substring(0,1) + endX(str.substring(1));
+      return str.substring(0, 1) + endX(str.substring(1));
     }
   }
 
@@ -33,13 +33,13 @@ public class Recursion{
       return "";
     // Recursive Step if there is 1 letter
     }else if (str.substring(1).equals("")) {
-      return str.substring(0,1) + stringClean(str.substring(1));
+      return str.substring(0, 1) + stringClean(str.substring(1));
     // Recursive Step if the first letter equals the second letter
-    }else if (str.substring(0,1).equals(str.substring(1,2))){
+    }else if (str.substring(0, 1).equals(str.substring(1, 2))){
       return stringClean(str.substring(1));
     // Recursive step if the first letter does not equal the second letter 
     }else{
-      return str.substring(0,1) + stringClean(str.substring(1));
+      return str.substring(0, 1) + stringClean(str.substring(1));
     } 
   }
 }
